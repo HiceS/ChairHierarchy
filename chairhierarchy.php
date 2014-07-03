dbd4513b37487d449b42936bb7d99f27853b315c
HiceS <shawnhice@gmail.com>
2014-07-02 17:48:07 -0700

    Completely converted old website
    Now i can begin making the point system whenever i get bored.

--- chairhierarchy.php
+++ chairhierarchy.php
@@ -1,8 +1,9 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>Escape Velocity</title>
+<title>Chair Hierarchy</title>
 <meta charset="utf-8">
+<link rel="icon" type="img/png" href="images/favicon.png" />
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400italic,700,900" rel="stylesheet">
 <script src="js/jquery-1.9.1.min.js"></script>
 <script src="chair_css/5grid/init.js?use=mobile,desktop,1000px&amp;mobileUI=1&amp;mobileUI.theme=none&amp;mobileUI.titleBarHeight=0"></script>
@@ -27,16 +28,16 @@
       <div class="12u">
         <div id="header">
           <div id="logo">
-            <h1><a href="#" class="mobileUI-site-name">Escape Velocity</a></h1>
-            <span class="byline">A free responsive site template by HTML5 UP</span></div>
+            <h1><a href="#" class="mobileUI-site-name">The Chair Hierarchy</a></h1>
+            <span class="byline">Version 2.0 and getting better</span></div>
           <nav id="nav" class="mobileUI-site-nav">
             <ul>
-              <li class="current_page_item"><a href="index.html">Home</a></li>
-              <li> <span>Dropdown</span>
+              <li class="current_page_item"><a href="chairhierarchy.php">Home</a></li>
+              <li> <span>User Options</span>
                 <ul>
-                  <li><a href="#">Lorem ipsum</a></li>
-                  <li><a href="#">Magna veroeros</a></li>
-                  <li><a href="#">Etiam nisl</a></li>
+                  <li><a href="#">Account info</a></li>
+                  <li><a href="#">Logout</a></li>
+				  <!--
                   <li> <span>Sed consequat</span>
                     <ul>
                       <li><a href="#">Lorem dolor</a></li>
@@ -46,12 +47,12 @@
                       <li><a href="#">Sed feugiat</a></li>
                     </ul>
                   </li>
-                  <li><a href="#">Nisl tempus</a></li>
+				  -->
                 </ul>
               </li>
-              <li><a href="right-sidebar.html">Right Sidebar</a></li>
-              <li><a href="left-sidebar.html">Left Sidebar</a></li>
-              <li><a href="no-sidebar.html">No Sidebar</a></li>
+              <li><a href="#">Chair Bio</a></li>
+              <li><a href="#">LeaderBoards</a></li>
+              <li><a href="#">How to Move up</a></li>
             </ul>
           </nav>
         </div>
@@ -60,92 +61,83 @@
   </div>
 </div>
 <div id="intro-wrapper" class="wrapper wrapper-style1">
-  <div class="title">The Introduction</div>
+  <div class="title">The Rules</div>
   <div class="5grid-layout">
     <div class="row">
       <div class="12u">
         <section id="intro">
           <p class="style1">So in case you were wondering what this is all about ...</p>
-          <p class="style2"> Escape Velocity is a free responsive<br class="mobileUI-blank">
-            site template by <a href="#" class="nobr">HTML5 UP</a> </p>
-          <p class="style3">It's <strong>responsive</strong>, built on <strong>HTML5</strong> and <strong>CSS3</strong>, and released for free under the Creative Commons Attribution 3.0 license, so use it for any of your personal or commercial projects &ndash; just be sure to credit us!</p>
-          <ul class="actions">
-            <li><a href="#" class="button button-style3 button-big">Proceed</a></li>
-          </ul>
+          <p class="style2">The Chair Hierarchy defines who is more obligated to sit in the very few chairs in the shop, it shall be followed now and always.</a> </p>
+		  <p class="style2">It is the Law Of The Chairs</a> </p>
+          <p class="style3">It's only <strong>fair</strong>, to <strong>You</strong> and <strong>Your</strong> fellow shop mates!</p>
         </section>
       </div>
     </div>
   </div>
 </div>
 <div class="wrapper wrapper-style2">
-  <div class="title">The Details</div>
+  <div class="title">The List</div>
   <div class="5grid-layout">
     <div class="row">
       <div class="12u">
-        <div id="main"> <a href="#" class="image image-featured"> <img src="images/pic01.jpg" alt=""></a>
-          <section id="features">
-            <header class="style1">
-              <h2>Dolor consequat feugiat amet veroeros</h2>
-              <p class="byline">Feugiat dolor nullam orci pretium phasellus justo</p>
-            </header>
-            <div class="feature-list">
-              <div class="5grid">
-                <div class="row">
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-comment">Mattis velit diam vulputate</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-refresh">Lorem ipsum dolor sit veroeros</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-picture">Pretium phasellus justo lorem</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-cog">Tempus sed pretium orci</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-wrench">Aliquam consequat et feugiat</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                  <div class="6u">
-                    <section>
-                      <h3 class="icon icon-check">Dolore laoreet aliquam mattis</h3>
-                      <p>Eget mattis at, laoreet vel et velit aliquam diam ante, aliquet sit amet vulputate. Eget mattis at, laoreet vel velit lorem.</p>
-                    </section>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <ul class="actions actions-centered">
-              <li><a href="#" class="button button-style1 button-big">Get Started</a></li>
-              <li><a href="#" class="button button-style2 button-big">More Info</a></li>
-            </ul>
-          </section>
+<!--     <div id="main"> <a class="image image-full" width="200px" height="200px"> <img src="images/seal_of_approval.jpg"></a>
+		</div> 	
+-->		
+          <div style="font-size: 10pt; font-weight: bold;">
+        	<div class="entry" align="center">
+			<section id="intro">
+			<p class="style4">
+          		<ol>
+            		<li>
+             			<strong>Carlo</strong>
+            		</li>
+            		<li>
+              			<strong>Misc... Mentors (not including Alex)</strong>
+            		</li>
+            		<li>
+              			<strong>Griffen</strong>
+            		</li>
+					<li>
+              			<strong>Girls (assuming they do not chair hog (verified by Shawn , Jackson , or Griffen))</strong>
+            		</li>
+                    <li>
+                      	<strong>Bryce - Ryan (cause we love them equally)</strong>
+                    </li>
+                    <li>
+                      	<strong>Programming Alumni</strong>
+                    </li>
+                    <li>
+                      	<strong>Jackson - Shawn (because he is awesome)</strong>
+                    </li>
+					<li>
+						<strong>Officers</strong>
+					</li>
+                    <li>
+                      	<strong>Crazy Alex (cause we are a little scared of him)</strong>
+                    </li>
+                    <li>
+                      	<strong>Lucas (cause he needs a break every once in a while :) )</strong>
+                    </li>
+                    <li>
+                      	<strong>New programming members</strong>
+                    </li>
+                    <li>
+                      	<strong>Returning members</strong>
+                    </li>
+                    <li>
+                      	<strong>New Members</strong>
+                    </li>
+          		</ol>
+			</section>
+			</p>
+        	  </div>
+    	    </div>
         </div>
       </div>
     </div>
-  </div>
-</div>
+ </div>
 <div class="wrapper wrapper-style3">
-  <div class="title">The Endorsements</div>
+  <div class="title">The Chairs</div>
   <div class="5grid-layout">
     <div class="row">
       <div class="12u">
@@ -153,30 +145,60 @@
           <div class="5grid">
             <div class="row">
               <div class="4u">
-                <section class="highlight highlight-one"> <a href="#" class="image image-full"><img src="images/pic02.jpg" alt=""></a>
-                  <h3><a href="#">Aliquam diam consequat</a></h3>
-                  <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-                  <ul class="actions">
-                    <li><a href="#" class="button button-style1">Learn More</a></li>
-                  </ul>
+                <section class="highlight highlight-one"> <a class="image image-full"><img src="images/chair1.jpg" alt=""></a>
+                  <h3><a>The best chair ever <br>
+				  (programmers' chair)</a></h3>
                 </section>
               </div>
               <div class="4u">
-                <section class="highlight highlight-two"> <a href="#" class="image image-full"><img src="images/pic03.jpg" alt=""></a>
-                  <h3><a href="#">Nisl adipiscing sed lorem</a></h3>
-                  <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-                  <ul class="actions">
-                    <li><a href="#" class="button button-style1">Learn More</a></li>
-                  </ul>
+                <section class="highlight highlight-two"> <a class="image image-full"><img src="images/chair2.jpg" alt=""></a>
+                  <h3><a>Arguably the most comfortable chair</a></h3>
                 </section>
               </div>
               <div class="4u">
-                <section class="highlight highlight-three"> <a href="#" class="image image-full"><img src="images/pic04.jpg" alt=""></a>
-                  <h3><a href="#">Mattis tempus lorem</a></h3>
-                  <p>Eget mattis at, laoreet vel amet sed velit aliquam diam ante, dolor aliquet sit amet vulputate mattis amet laoreet lorem.</p>
-                  <ul class="actions">
-                    <li><a href="#" class="button button-style1">Learn More</a></li>
-                  </ul>
+                <section class="highlight highlight-three"> <a class="image image-full"><img src="images/chair3.jpg" alt=""></a>
+                  <h3><a>Carlo's GoodWill chair</a></h3>
+                </section>
+              </div>
+            </div>
+			<div class="row">
+              <div class="4u">
+                <section class="highlight highlight-one"> <a class="image image-full"><img src="images/chair4.jpg" alt=""></a>
+                  <h3><a>Autodesk Beanbag</a></h3>
+                </section>
+              </div>
+              <div class="4u">
+                <section class="highlight highlight-two"> <a class="image image-full"><img src="images/chair5.jpg" alt=""></a>
+                  <h3><a>The short chair</a></h3>
+                </section>
+              </div>
+              <div class="4u">
+                <section class="highlight highlight-three"> <a class="image image-full"><img src="images/chair6.jpg" alt=""></a>
+                  <h3><a>The absolute worst rolling chair ever</a></h3>
+                </section>
+              </div>
+            </div>
+			<div class="row">
+              <div class="4u">
+                <section class="highlight highlight-one"> <a class="image image-full"><img src="images/chair7.jpg" alt=""></a>
+                  <h3><a>The blue, cracked, plastic chair</a></h3>
+                </section>
+              </div>
+              <div class="4u">
+                <section class="highlight highlight-two"> <a class="image image-full"><img src="images/chair8.jpg" alt=""></a>
+                  <h3><a>The pink, cracked, plastic chair</a></h3>
+                </section>
+              </div>
+              <div class="4u">
+                <section class="highlight highlight-three"> <a class="image image-full"><img src="images/chair9.jpg" alt=""></a>
+                  <h3><a>The hard, uncomfortable chair</a></h3>
+                </section>
+              </div>
+            </div>
+			<div class="row">
+              <div class="4u">
+                <section class="highlight highlight-one"> <a class="image image-full"><img src="images/chair10.jpg" alt=""></a>
+                  <h3><a>The stool (back breaker)</a></h3>
                 </section>
               </div>
             </div>
